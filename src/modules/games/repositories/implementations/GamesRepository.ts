da69e1957ba2234821ef72a24af569553d8c5fbf
Pedro Tassinari <pedro.tassinari99@gmail.com>
2021-11-11 11:30:25 -0300

    finish challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,21 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
+      .where(`title ILIKE '%${param}%'`)
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT COUNT(id) FROM games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .relation('users')
+      .of(id)
+      .loadMany()
       // Complete usando query builder
   }
 }
